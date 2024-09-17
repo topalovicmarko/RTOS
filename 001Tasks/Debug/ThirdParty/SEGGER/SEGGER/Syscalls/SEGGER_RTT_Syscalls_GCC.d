39e6ac9ee78fd895b9dc79c21a370bfc07d7f788
topalovicmarko <marko.topalovic99@gmail.com>
2024-09-18 00:46:54 +0200

    SEGGER

---
+++ 001Tasks/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
@@ -0,0 +1,6 @@
+ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
+ ../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
+ D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
