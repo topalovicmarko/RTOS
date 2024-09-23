ab26f337b6d9079f0e5554e83c3e190dac8c693e
topalovicmarko <marko.topalovic99@gmail.com>
2024-09-23 13:18:16 +0200

    New exercies with vTaskDelayUntil

---
+++ 004LED_Periodic_Tasks/Debug/common/ThirdParty/FreeRTOS/event_groups.d
@@ -0,0 +1,42 @@
+common/ThirdParty/FreeRTOS/event_groups.o: \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/event_groups.c \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config/Global.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/portable.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/list.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/timers.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/event_groups.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/timers.h
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config/Global.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/portable.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/list.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/timers.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/event_groups.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/timers.h:
