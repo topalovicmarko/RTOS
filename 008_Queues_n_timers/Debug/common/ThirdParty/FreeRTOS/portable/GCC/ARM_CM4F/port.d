6d02843b9d1c95e486e95e160dc9cc8b06dc5f88
topalovicmarko <marko.topalovic99@gmail.com>
2024-09-25 18:25:39 +0200

    Queues

---
+++ 008_Queues_n_timers/Debug/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.d
@@ -0,0 +1,20 @@
+common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.o: \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.c \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/portable.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h \
+ D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/list.h
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/portable.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h:
+D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include/list.h:
